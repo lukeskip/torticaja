193f1a2cf4956a90f69a26ddf2c901906f6638a6
Cheko Garcia <contacto@chekogarcia.com.mx>
2023-03-15 18:00:39 -0600

    sass organization

--- resources/views/config.blade.php
+++ resources/views/config.blade.php
@@ -1,11 +1,14 @@
 @extends('layouts.app')
 @section('content')
 
-<form action="" >
+<form action="" class="config-sheet">
     <div class="boolean" data-label="Gas" data-id="gas" data-init="1"></div>
     <div class="boolean" data-label="Kilo por peso" data-id="kg-peso" data-init="0"></div>
     <div class="boolean" data-label="Esto es otro boton y es más larga la descripción" data-id="larga" data-init="0"></div>
-
+    <div class="form-group">
+        <label for="">Precio Harina</label>
+        <input type="text">
+    </div>
     
 </form>
 
